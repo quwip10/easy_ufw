60beb6f99321539bcd0013983d9d65791c658a3c
Alex Heidenreich <atlasalex@Alexs-MacBook-Air.local>
2019-03-02 16:02:31 -0500

    Added lots of comments

--- interactiveUFWfork.sh
+++ interactiveUFWfork.sh
@@ -15,16 +15,24 @@ fi
 
 printf "\nInteractive UFW Cool Stuff\n"
 
+#Checks for local file with preconfigured rules for automation
+#I need to add this procedure later
+#[insert code here]
+
+#Loops through showing the ufw rule status
+#Continues prompting user to enter rules until cont == 'n'
 until [ $cont == 'n' ];
 do
 	printf "\nCurrent UFW Status:\n"
-  printf "\nPress [enter] for next line"
+  printf "\nPress [enter] for next line\n"
   printf "Press [space] for next page\n"
   printf "Press q to exit status view\n"
-  read -p "Press [enter] to continue "
+  read -p "\nPress [enter] now to continue "
 
+  #displays current ufw status/rules piped to less
 	ufw status numbered | less
 
+  #new rule prompts
 	printf "\nEnter Source address or leave blank for any: "
 	read source
 
@@ -50,6 +58,7 @@ do
 	printf "\nEnter any comments or leave blank: "
 	read comments
 
+  #prints variables for rule
 	if [ -z "$protocol" ] && [ -z "$port" ];
 	then
 		printf "ufw allow from $source to $destination comment \"\'$comments \'\""
@@ -60,6 +69,11 @@ do
 		printf "ufw allow from $source to $destination port $port proto $protocol comment \"\'$comments \'\""
 	fi
 
+  #Prompt to commit rule
+#  printf "Commit rule? (y/n)"
+#  read commit
+
+  #Prompts user to add another rule. Quits on no.
 	printf "\nEnter another rule y/n? "
 	read cont
 	cont=${cont:0:1}
