eb3700c766df470d88124abd41822cf517e804ce
Alex Heidenreich <atlasalex@Alexs-MacBook-Air.local>
2019-03-02 15:24:02 -0500

    added sleep after UFW status

--- interactiveUFW.sh
+++ interactiveUFW.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 #Easy front end for enabling UFW on Virtual Servers
- 
+
 #Test for root priveleges
 if [ $EUID -ne 0 ];
 then
          printf "\nPlease run with sudo/root priveleges. Exiting...\n\n"
          sleep 1
-         exit 
+         exit
 fi
- 
+
 printf "Interactive UFW Cool Stuff\n"
- 
+
 printf "\nCurrent UFW Status:\n"
 ufw status numbered
+sleep 2
 
 printf "\nEnter Source address or leave blank for any: "
 read source
- 
+
 if [ -z "$source" ];
 then
 	source="any"
 fi
- 
+
 printf "\nEnter Destination address or leave blank for any: "
 read destination
- 
+
 if [ -z "$destination" ];
 then
 	destination="any"
@@ -39,7 +40,7 @@ read protocol
 
 printf "\nEnter any comments or leave blank: "
 read comments
- 
+
 if [ -z $protocol ] && [ -z $port];
 then
 	printf "ufw allow from $source to $destination comment \"\'$comments \'\""
